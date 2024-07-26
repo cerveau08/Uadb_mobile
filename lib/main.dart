0acf249241bf1bf3e8dea349064c7c07c277267d
MALICK COLY <55498348+cerveau08@users.noreply.github.com>
2024-07-26 20:16:29 +0000

    Inialisation et implementation du appBar

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Exemple 1',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Exemple 1'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Icon _icon1 = Icon(Icons.favorite_border, color: Colors.white);
+  Icon _icon2 = Icon(Icons.favorite_border, color: Colors.yellow);
+  bool _aimer = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+        actions: <Widget>[
+          IconButton(icon: _icon1, onPressed: cliquer),
+          IconButton(icon: _icon2, onPressed: cliquer)
+        ],
+      ),
+    );
+  }
+
+  void cliquer() {
+    setState(() {
+      if (_aimer) {
+        _aimer = false;
+        _icon1 = Icon(Icons.favorite, color: Colors.yellow);
+        _icon2 = Icon(Icons.favorite, color: Colors.white);
+      } else {
+        _aimer = true;
+        _icon1 = Icon(Icons.favorite_border, color: Colors.white);
+        _icon2 = Icon(Icons.favorite_border, color: Colors.yellow);
+      }
+    });
+  }
+}
