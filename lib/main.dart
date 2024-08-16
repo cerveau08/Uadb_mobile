0fac436bdf77ffc30ab583cab9586c7e6fb8a28f
MALICK COLY <55498348+cerveau08@users.noreply.github.com>
2024-08-16 20:56:10 +0000

    implementation d'un image et du drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,18 +7,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Exemple 1',
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Exemple 1'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -32,61 +31,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Icon _icon1 = const Icon(Icons.favorite_border, color: Colors.white);
-  Icon _icon2 = const Icon(Icons.favorite_border, color: Colors.yellow);
-  bool _aimer = false;
+  int _indexSelector = 0;
+  String _affichage = '0 Alarm';
+
+  void changeAffichage(int index) {
+    setState(() {
+      _indexSelector = index;
+      switch (_indexSelector) {
+        case 0:
+          _affichage = '$_indexSelector Alarm';
+          break;
+        case 1:
+          _affichage = '$_indexSelector Home';
+          break;
+        case 2:
+          _affichage = '$_indexSelector Foward';
+          break;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.blue,
-        actions: <Widget>[
-          IconButton(icon: _icon1, onPressed: cliquer),
-          IconButton(icon: _icon2, onPressed: cliquer)
-        ],
+        backgroundColor: Colors.green,
+        title: const Text('un AppBar'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: cliquer,
-        backgroundColor: Colors.blue,
-        tooltip: 'Next',
-        shape: const CircleBorder(),
-        elevation: 20.0,
-        child: const Icon(Icons.forward),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      bottomNavigationBar: BottomAppBar(
-          color: Colors.blue,
-          child: Container(
-            height: 50,
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Icon(
-                  Icons.home,
-                  semanticLabel: "Home",
-                ),
-                Icon(Icons.search),
-                Icon(Icons.notifications),
-                Icon(Icons.account_circle),
-              ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              _affichage,
+              style: const TextStyle(fontSize: 40),
             ),
-          )),
+            Image.asset(
+              "images/cachet.png",
+              width: 90,
+            )
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.green,
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Image.asset(
+                'images/cachet.png',
+                width: 100,
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.alarm),
+              title: Text('Alarm'),
+            ),
+            const ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            const ListTile(
+              leading: Icon(Icons.forward),
+              title: Text('Fowards'),
+              trailing: Icon(Icons.forward),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm, color: Colors.white),
+            label: 'Alarm',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.white),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.forward, color: Colors.white),
+            label: 'Foward',
+          )
+        ],
+        backgroundColor: Colors.green,
+        onTap: changeAffichage,
+        currentIndex: _indexSelector,
+      ),
     );
-  }
-
-  void cliquer() {
-    setState(() {
-      if (_aimer) {
-        _aimer = false;
-        _icon1 = const Icon(Icons.favorite, color: Colors.yellow);
-        _icon2 = const Icon(Icons.favorite, color: Colors.white);
-      } else {
-        _aimer = true;
-        _icon1 = const Icon(Icons.favorite_border, color: Colors.white);
-        _icon2 = const Icon(Icons.favorite_border, color: Colors.yellow);
-      }
-    });
   }
 }
